da967be1d479e5cf6665a7d2728b7874cd8beb39
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-03-17 00:17:52 +0100

    Add swagger dependency and set up

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 var express = require("express"),
 	bodyParser = require("body-parser"),
+    swaggerJSDoc = require("swagger-jsdoc");
 	mongoOp = require("./models/mongo");
 
 var app = express();
+// Swagger definition
+var swaggerDefinition = {
+    info: {
+        title: 'API de gestión de usuarios',
+        version: '1.0.0',
+        description: 'Descripción del API del servicio de usuarios'
+    },
+    host: 'localhost:3000',
+    basePath: '/'
+};
 
+// Options for the swagger docs
+var options = {
+  // Import swaggerDefinitions
+    swaggerDefinition: swaggerDefinition,
+    // Path to the API docs
+    apis: ['*.js']
+};
+
+// Initialize swagger-jsdoc
+var swaggerSpec = swaggerJSDoc(options);
 
 app.use(express.static('./public'));
 
@@ -14,6 +35,12 @@ require('./routes')(app);
 
 app.use('/', function(req, res) {
 	console.log("Hello World");
+});
+
+// Serve swagger
+app.get('/swagger.json', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
 });
 
 app.listen(3000);
