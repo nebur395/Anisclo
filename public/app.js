cf96d6bbb459e5166f4518ae57c23c16cc711362
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-03-17 11:40:38 +0100

    Add login and signUp states

--- public/app.js
+++ public/app.js
@@ -3,12 +3,36 @@ angular.module('pirineoPOIApp', ['ui.router', 'base64'])
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
 
-            //starter screen
-            .state('starter', {
-                url: "/starter",
-                templateUrl: "templates/starter.html",
-                controller: "starterCtrl"
-            });
+        //starter screen
+        .state('starter', {
+            url: "/starter",
+            templateUrl: "templates/starter.html",
+            controller: "starterCtrl"
+        })
+
+        //login screen
+        .state('login', {
+            url: "/login",
+            templateUrl: "templates/login.html",
+            controller: "loginCtrl",
+            onEnter: function($state, auth){
+                if(auth.isAuthenticated()){
+                    $state.go('starter');
+                }
+            }
+        })
+
+        //sign up screen
+        .state('signUp', {
+            url: "/signUp",
+            templateUrl: "templates/signUp.html",
+            controller: "signUpCtrl",
+            onEnter: function($state, auth){
+                if(auth.isAuthenticated()){
+                    $state.go('starter');
+                }
+            }
+        });
 
         $urlRouterProvider.otherwise('starter');
     });
