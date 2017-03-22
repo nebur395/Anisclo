35a4b2d1adf88bd5e23a467235b45201ea27eb92
dari1495 <dari1495@gmail.com>
2017-03-22 20:52:31 +0100

    Added HTTPS support to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require("express"),
 	bodyParser = require("body-parser"),
     mongoose = require('mongoose'),
-    swaggerJSDoc = require("swagger-jsdoc");
+    swaggerJSDoc = require("swagger-jsdoc"),
+    crypto = require("crypto"),
+    fs = require("fs")
+    https = require("https");
 
 
 var app = express();
@@ -47,14 +50,26 @@ app.get('/swagger.json', function(req, res) {
     res.send(swaggerSpec);
 });
 
+// Creation of https connection
+var privateKey = fs.readFileSync('localhost.key','utf8');
+var certificate = fs.readFileSync('localhost.crt','utf8');
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials,app);
+
+
 // Database connection and server launching
 var dbUri = 'mongodb://localhost:27017/aniscloDb';
 mongoose.connect(dbUri);
 mongoose.connection.once('open', function(){
 
     console.log("MongoDB connection created in "+dbUri);
+
     app.listen(8080, function(){
         console.log("Server listening to PORT 8080");
+    });
+    //HTTPS server launch (compatible with http at the same time)
+    httpsServer.listen(8443,function () {
+        console.log("Secure server listening to PORT 8443");
     });
 
 });
