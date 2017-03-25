f7c7cda915d2c94511bd80d5476616d641bb7df1
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-03-25 19:08:41 +0100

    Add angular-google-maps external module

--- public/app.js
+++ public/app.js
@@ -1,4 +1,12 @@
-angular.module('pirineoPOIApp', ['ui.router', 'base64', 'vcRecaptcha'])
+angular.module('pirineoPOIApp', ['ui.router', 'base64', 'vcRecaptcha', 'uiGmapgoogle-maps'])
+
+    .config(function(uiGmapGoogleMapApiProvider) {
+        uiGmapGoogleMapApiProvider.configure({
+            // key: 'API KEY HERE',
+            v: '3.20', //defaults to latest 3.X anyhow
+            libraries: 'weather,geometry,visualization'
+        });
+    })
 
     .config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
