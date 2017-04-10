31e4e23bbe56f04fe8227a1eb3810331ffb249b4
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-04-10 16:46:37 +0200

    Added list of pois section

--- public/javascript/controllers/starterController.js
+++ public/javascript/controllers/starterController.js
@@ -3,30 +3,35 @@ angular.module('pirineoPOIApp')
     .controller('starterCtrl', ['$scope', '$state', 'auth', 'uiGmapGoogleMapApi',
 
         function ($scope, $state, auth, uiGmapGoogleMapApi) {
+
+            $scope.poiList = ["blabla","blabla","blabla","blabla","blabla","blabla","blabla","blabla","blabla",
+                "blabla","blabla","blabla","blabla","blabla","blabla","blabla","blabla"];
+
+            // MAP SECTION
+
             $scope.map = {
-                center: { latitude: 45, longitude: -73 }, zoom: 8,
+                center: {latitude: 45, longitude: -73}, zoom: 8,
 
                 markers: [],
                 events: {
-                    click: function(mapModel, eventName, originalEventArgs)
-            {
-                $scope.$apply(function(){
-                    var e = originalEventArgs[0];
-                    var marker = {};
-                    marker.coords = {};
-                    marker.coords.latitude = e.latLng.lat();
-                    marker.coords.longitude = e.latLng.lng();
-                    console.log("Saving marker: "+marker.coords.latitude + " ; "+marker.coords.longitude);
-                    $scope.map.markers.push(marker);
-                });
-                //TODO: añadir aquí llamada a funcion mostrar formulario
-            }
+                    click: function (mapModel, eventName, originalEventArgs) {
+                        $scope.$apply(function () {
+                            var e = originalEventArgs[0];
+                            var marker = {};
+                            marker.coords = {};
+                            marker.coords.latitude = e.latLng.lat();
+                            marker.coords.longitude = e.latLng.lng();
+                            console.log("Saving marker: " + marker.coords.latitude + " ; " + marker.coords.longitude);
+                            $scope.map.markers.push(marker);
+                        });
+                        //TODO: añadir aquí llamada a funcion mostrar formulario
+                    }
                 }
             };
-            $scope.options = { scrollwheel: false, streetViewControl:false, mapTypeControl:false};
-            uiGmapGoogleMapApi.then(function(maps) {
+            $scope.options = {scrollwheel: false, streetViewControl: false, mapTypeControl: false};
+            uiGmapGoogleMapApi.then(function (maps) {
 
 
             });
 
-    }]);
+        }]);
