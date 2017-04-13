308ba0bc5058a835442903764303175b4e312ba5
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-04-13 03:46:27 +0200

    Added view infraestructure of list favourites and list follows functionalities

---
+++ public/javascript/controllers/favsListController.js
@@ -0,0 +1,8 @@
+angular.module('pirineoPOIApp')
+
+    .controller('favsListCtrl', ['$scope', 'auth', function ($scope, auth) {
+
+        /*
+        TODO DARÍO: la función auth.getFavs() ya te devuelve el array con todos los favoritos actuales
+         */
+    }]);
---
+++ public/javascript/controllers/followListController.js
@@ -0,0 +1,8 @@
+angular.module('pirineoPOIApp')
+
+    .controller('followListCtrl', ['$scope', 'auth', function ($scope, auth) {
+
+        /*
+         TODO DARÍO: la función auth.getFollows() ya te devuelve el array con todos a los que sigues actualmente
+         */
+    }]);
