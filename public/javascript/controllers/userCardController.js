41bfa4d328d8d3e8ace535d334ff590480dff0e8
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-05-02 02:16:33 +0200

    Added modalSubmit function schema

--- public/javascript/controllers/userCardController.js
+++ public/javascript/controllers/userCardController.js
@@ -18,4 +18,24 @@ angular.module('pirineoPOIApp')
                 Notification.success('&#10004' + message);
             };
 
+            $scope.modalButton = 0;
+            $scope.modalSubmit = function () {
+                switch ($scope.modalButton) {
+                    case 0:
+                        showSuccess("0");
+                        break;
+                    case 1:
+                        showSuccess("1");
+                        break;
+                    case 2:
+                        showSuccess("2");
+                        break;
+                    case 3:
+                        $scope.editing = !$scope.editing;
+                        break;
+                    default:
+                        showSuccess("4");
+                }
+            };
+
         }]);
